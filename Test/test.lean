bb3be04b98fdb77832d5e659f3fd664ccb2a5ac5
Shahrizoda Kenjaboyeva <shahrizoda.kenjaboyeva@gmail.com>
2026-03-23 10:52:18 +0500

    Delete Test directory

--- Test/test.lean
+++
@@ -1,8 +0,0 @@
--- My first proofs in Lean 4
-
-theorem add_comm_test : 1 + 2 = 2 + 1 := by rfl
-
-theorem and_intro (p q : Prop) (hp : p) (hq : q) : p ∧ q := by
-  exact ⟨hp, hq⟩
-
-#check Nat.add_comm
